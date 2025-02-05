93b4dff6c5199909c580f16419310e3b7fcdce8d
Alejandro E. Rendon <alejandro@rendon.co>
2025-02-05 06:25:19 -0500

    Create configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BRANCH === "main" ? "/website-2025/" : "/website-develop/",
   build: {
     outDir: "dist",
   },
