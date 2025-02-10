3c9eff7f1fe025ddb503188037d30167b1b1df33
Alejandro E. Rendon <alejandro@rendon.co>
2025-02-09 19:24:06 -0500

    Setup module

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import process from 'process';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.BRANCH === 'main' ? '/website-2025/' : '/website-develop/',
+  base: process.env.BRANCH === 'main' ? '/website-2025/' : '/website-develop/',
   build: {
     outDir: 'dist'
   }
