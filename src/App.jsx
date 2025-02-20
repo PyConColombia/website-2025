4c34581df60bfb72a0fc47246c9c9b3d0eab947a
Alejandro E. Rendon <alejandro@rendon.co>
2025-02-19 22:02:47 -0500

    Configura GA4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+
 import LandingPage from '@/pages/LandingPage';
 import Layout from '@/layout';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/main.css';
 
+const TRACKING_ID = 'G-J2DYL7NXX5';
+
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+  }, []);
+
   return (
     <Layout>
       <LandingPage />
